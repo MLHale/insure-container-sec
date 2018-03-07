34759c560e17f408220b4820c4b34f365113dc82
danielritter14 <danielritter14@gmail.com>
2018-03-06 20:52:39 -0600

    update scenario four

---
+++ scenario_four/vm1/container1/scp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp /root/testfile root@container4.cont:/root/testfile
--- scenario_four/vm1/setup.sh
+++ scenario_four/vm1/setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./setup.sh [ip of second vm running on same hypervisor] [port on vm that forwards to ssh service on container ]"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./setup.sh [ip of second vm running on same hypervisor] [port on vm that forwards to ssh service on container ] [ip of third vm running on different hypervisor] [port on last vm]"
+    exit
 fi
 
-echo "#!/bin/bash" > container1/container2.sh
-echo ssh -t $1 -p $2 /root/container3.sh >> container1/container2.sh
+echo Host container3.cont > container1/config
+echo "  User          root" >> container1/config
+echo "  HostName      container3.cont" >> container1/config
+echo "  ProxyCommand  ssh root@$1 -p $2 -W %h:%p" >> container1/config
+
+echo Host $3 >> container1/config
+echo "  User          root" >> container1/config
+echo "  HostName      $3" >> container1/config
+echo "  ProxyCommand  ssh root@container3.cont -W %h:%p" >> container1/config
+
+echo "#!/bin/bash" > container1/scp.sh
+echo scp -P $4 /root/testfile root@$3:/root/testfile >> container1/scp.sh
 
 cd container1/ && make build && make start
