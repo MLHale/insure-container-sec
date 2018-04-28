0767ea27bc8a72172c556e18373d7e07b7f615a0
abohac <35820595+abohac@users.noreply.github.com>
2018-04-28 16:05:25 -0500

    Create openvas.sh

---
+++ container_scanner/openvas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Ensure all tasks are deleted before running
+#Target, user, and password have already been established
+
+#Get Target UUID
+TUUID=$(omp -u admin -w new_password -T  | grep test123 | cut -d ' ' -f1 | tr -d '\n')
+
+#Create Task -> always use same config UUID, hard coded
+omp -u admin -w new_password -C --target=$TUUID -c daba56c8-73ec-11df-a475-002264764cea -n "new_scan"
+
+#Find task ID
+TASKID=$(omp -u admin -w new_password -G | grep "new_scan" | cut -d ' ' -f1)
+
+#Start Task
+T=$(omp -u admin -w new_password -S $TASKID)
+
+T2=$(omp -u admin -w new_password --get-tasks --details | sed -n 2p | cut -d ' ' -f3)
+echo $T2
+
+#get HTML UUID
+RUUID=$(omp -F -u admin -w new_password | grep HTML | cut -d ' ' -f1)
+
+#Generate Report
+omp -u admin -w new_password -R $T2 -f $RUUID
